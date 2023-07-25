358970425ec40663616f66fa44404c96b198b44c
camillanovaglio <camilla.novaglio@gmail.com>
2023-07-25 08:46:03 +0000

    Refine plots colour scale and file format

--- R/Plotting_functions_SO.r
+++ R/Plotting_functions_SO.r
@@ -9,7 +9,9 @@ plot_SO<-function(wmap_df = antmap_df,
                   legend_name = legend_name,
                   min = min, 
                   max = max,
-                  data_type = "modelled"){
+                  data_type = "modelled", 
+                  color_scale = color_scale, 
+                  theme_map = my_theme_map){
   
   # # # trial
   # wmap_df = antmap_df
@@ -17,9 +19,10 @@ plot_SO<-function(wmap_df = antmap_df,
   # proj = circumpolarCRS
   # legend_name = "g C m-2"
   # min = 0
-  # max = 0.1
+  # max = 8.75
   # data_type = "phyto"
-  
+  # color_scale = color_scale
+
   # convert rasterLayer to sf object and consider circumpolar projections within function
   data_to_plot <- rasterToPolygons(data_to_plot) # convert Rasterlayer to spatial polygon dataframe 
   data_to_plot <- st_as_sf(data_to_plot) # convert dataframe to sf object 
@@ -37,48 +40,64 @@ plot_SO<-function(wmap_df = antmap_df,
     breaks_vector = c(0,0.5,1,2,3,5,10,25)
     labels_vector = c(0,0.5,1,2,3,5,10,25)
   }else if (data_type == "phyto"){
-    breaks_vector = c(0,0.5,1.25,2.5,3.75,5,6.25,8.75)
-    labels_vector = c(0,0.5,1.25,2.5,3.75,5,6.25,8.75)
+    breaks_vector = c(0,0.5,1.2,2.5,3.7,5.0,6.2,8.7)
+    labels_vector = c(0,0.5,1.2,2.5,3.7,5.0,6.2,8.7)
   }else{
     breaks_vector = seq(min, max, length.out = 5)
     labels_vector = format(round(seq(min, max, length.out = 5), digits = 2), nsmall = 2)
   }
   
-  p1<-ggplot() +
-    geom_polygon(data=antmap_df, aes(x=long, y=lat, group=group), fill="grey40", color=NA, linewidth = 0.25) +
-    geom_sf(data=data_to_plot, colour = NA, aes(fill = index_1))+
+  if(color_scale == "viridis"){
     
-    # # scale_fill_viridis_d is for discrete, c is for continuous and b is for binned
-    # scale_fill_viridis_b(guide = guide_colorbar(
-    #   title = legend_name,
-    #   title.position = "top",
-    #   title.hjust = 0.5),
-    #   option = "D",
-    #   breaks = breaks_vector,
-    #   labels = labels_vector,
-    #   limits=c(min, max),
-    #   oob = scales::squish,
-    #   na.value = "black" # not working
-    # ) +
+    p1<-ggplot() +
+      # geom_polygon(data=antmap_df, aes(x=long, y=lat, group=group), fill="grey40", color=NA, linewidth = 0.25) +
+      geom_sf(data=data_to_plot, colour = NA, aes(fill = index_1))+
+      geom_polygon(data=antmap_df, aes(x=long, y=lat, group=group), fill="grey40", color=NA, linewidth = 0.25) +
+      
+      # scale_fill_viridis_d is for discrete, c is for continuous and b is for binned
+      scale_fill_viridis_b(guide = guide_colorbar(
+        title = legend_name,
+        title.position = "top",
+        title.hjust = 0.5),
+        option = "D",
+        breaks = breaks_vector,
+        labels = labels_vector,
+        limits=c(min, max),
+        oob = scales::squish,
+      ) +
+      
+      scale_x_continuous(expand=c(0,0))+ # this should get rid of the white space around the map... 
+      scale_y_continuous(expand=c(0,0))
   
-    # https://colorbrewer2.org/#type=sequential&scheme=Blues&n=3
-    scale_fill_gradient(guide = guide_colorbar(
-      title = legend_name,
-      title.position = "top",
-      title.hjust = 0.5),
-      low='#deebf7', 
-      high='#3182bd',
-      breaks = breaks_vector,
-      labels = labels_vector,
-      limits=c(min, max),
-      oob = scales::squish,
-      na.value = "black" )+
+    }else if(color_scale == "monochromatic"){
+      
+      p1<-ggplot() +
+        # geom_polygon(data=antmap_df, aes(x=long, y=lat, group=group), fill="grey40", color=NA, linewidth = 0.25) +
+        geom_sf(data=data_to_plot, colour = NA, aes(fill = index_1))+
+        geom_polygon(data=antmap_df, aes(x=long, y=lat, group=group), fill="grey40", color=NA, linewidth = 0.25) +
     
-    scale_x_continuous(expand=c(0,0))+ # this should get rid of the white space around the map... 
-    scale_y_continuous(expand=c(0,0))+
-    my_theme_map
+        # https://colorbrewer2.org/#type=sequential&scheme=Blues&n=3
+        scale_fill_gradient(guide = guide_colorbar(
+         title = legend_name,
+          title.position = "top",
+          title.hjust = 0.5),
+          low='#deebf7', 
+          high='#3182bd',
+          breaks = breaks_vector,
+          labels = labels_vector,
+          limits=c(min, max),
+          oob = scales::squish)+
+    
+        scale_x_continuous(expand=c(0,0))+ # this should get rid of the white space around the map... 
+        scale_y_continuous(expand=c(0,0))
+      
+    }
   
-  p1
+  if(theme_map == "my_theme_map"){
+    p1 = p1 + my_theme_map
+  }else if (theme_map == "my_theme_map2"){
+    p1 = p1 + my_theme_map2
+  }
   
   return (p1 = p1)
   
